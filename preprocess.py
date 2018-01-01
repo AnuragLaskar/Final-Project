cf4cf8190e77ef655d83d585b1047f1c71626151
MaxySpark <hello@maxyspark.com>
2018-01-02 04:31:14 +0530

    najanu

--- preprocess.py
+++ preprocess.py
@@ -4,4 +4,5 @@ def rem_symbol(word):
     c_word = re.sub('[^a-zA-Z0-9]','',word)
     return c_word
 
-text = "he'sllo.ss iugu@i$gu ^ *SKD(SDSD+-_)SSDSD guyg!u#gu"
+def clean_tweet(tweet):
+	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
