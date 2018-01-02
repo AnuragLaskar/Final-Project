9585c51d0c43df824fe050c663a4167613a12a99
MaxySpark <hello@maxyspark.com>
2018-01-02 13:33:11 +0530

    up

--- preprocess.py
+++ preprocess.py
@@ -6,3 +6,19 @@ def rem_symbol(word):
 
 def clean_tweet(tweet):
 	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+ 
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+ 
+    return False
