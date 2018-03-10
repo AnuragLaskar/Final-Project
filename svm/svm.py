3d5f95f4277c007916afdbb2bf31097cdcd27b83
MaxySpark <hello@maxyspark.com>
2018-03-10 09:12:03 +0530

    update

--- svm/svm.py
+++ svm/svm.py
@@ -15,6 +15,10 @@ df_x = df['text']
 df_y = df['senti']
 x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=4)
 
+print "Total Datasets %d" % (len(df))
+print "Total Training Datasets %d" % (len(x_train))
+print "Total Testing Datasets %d" % (len(x_test))
+
 # read stop word from data and add to stop_words list
 with open("../Data/Classifier/stop-word.txt") as f:
     stop_word_list = f.readlines()
@@ -23,12 +27,14 @@ stop_word_list = [x.strip() for x in stop_word_list]
 cv = TfidfVectorizer(min_df=1,stop_words=stop_word_list)
 
 x_traincv = cv.fit_transform(x_train)
-a=x_traincv.toarray()
+# a=x_traincv.toarray()
 # cv.inverse_transform(a[0])
 
 y_train=y_train.astype('int')
 
 x_testcv=cv.transform(x_test)
+
+print "Traning is In Progress Please Wait"
 
 clf = svm.SVC(kernel='linear',C=1.0)
 clf.fit(x_traincv,y_train)
@@ -44,4 +50,5 @@ for i in range (len(predictions)):
         count=count+1
 
 acc = count/len(predictions)
-print "Accuracy : %f " % acc
+acc*=100
+print "Accuracy : %f %% " % acc
